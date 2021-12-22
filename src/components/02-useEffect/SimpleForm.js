98d2dedb9b9428fc1895535eaf1180b699e1016c
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2021-12-21 21:48:20 -0600

    Add use effect to form

--- src/components/02-useEffect/SimpleForm.js
+++ src/components/02-useEffect/SimpleForm.js
@@ -1,12 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './effects.css';
 
 export const SimpleForm = () => {
+    const [formState, setFormState] = useState({
+        name: '',
+        mail: ''
+    });
+    
+    const { name, mail } = formState;
+    
+    useEffect(() => {
+       console.log('default'); 
+    }, []);
+    
+    useEffect(() => {
+        console.log('formstate'); 
+     }, [formState]);
+     
+     useEffect(() => {
+        console.log('mail'); 
+     }, [mail]);
+    
+    const handleInputChange = ({target}) => {
+      setFormState({
+          ...formState, 
+          [target.name]: target.value
+      });
+    };
+    
     return (
         <>
          <h1>Use Effect</h1>   
          <hr />
          
+         <div className='form-group'>
+             <input 
+                type="text" 
+                name="name"
+                className='form-control'
+                placeholder='Your name'
+                autoComplete='off'
+                value={name}
+                onChange={handleInputChange}>
+             </input>
+             
+             <br />
+             
+             <input 
+                type="text" 
+                name="mail"
+                className='form-control'
+                placeholder='Your email'
+                autoComplete='off'
+                value={mail}
+                onChange={handleInputChange}>
+             </input>
+         </div>
         </>
     )
 }
