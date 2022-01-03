89fd2a8e18eaf8579b1bf1737c000b01e10dfdfc
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2022-01-03 15:00:36 -0600

    finish useLayoutEffect

--- src/components/05-useLayoutEffect/Layout.js
+++ src/components/05-useLayoutEffect/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useLayoutEffect, useRef, useState } from 'react'
 import { useCounter } from '../../hooks/useCounter';
 import { useFetch } from '../../hooks/useFetch'
 import './layout.css';
@@ -10,16 +10,28 @@ export const Layout = () => {
     
     const { quote } = !!data && data[0];
     
+    const pTag = useRef();
+    const [boxSize, setBoxSize] = useState({});
+    
+    useLayoutEffect(() => {
+        console.log('hey', pTag.current.getBoundingClientRect().width);
+        setBoxSize(pTag.current.getBoundingClientRect());
+    }, [quote])
+    
     return (
         <div>
             <h1>Focus Screen</h1>
             <hr />
             
             <blockquote className='blockquote text-right'>
-                <p>
+                <p ref={pTag}>
                     {quote}
                 </p>
             </blockquote>
+            
+            <pre>
+                {JSON.stringify(boxSize, null, 3)}
+            </pre> 
             
             <button className='btn btn-primary' 
                     onClick={increment}>
