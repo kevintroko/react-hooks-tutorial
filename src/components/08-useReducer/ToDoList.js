e2dcf4bd137e2fefb1a317121e9840d6d8e9a985
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2022-01-04 22:11:55 -0600

    Move todo list to its own component

--- src/components/08-useReducer/ToDoList.js
+++ src/components/08-useReducer/ToDoList.js
@@ -1,24 +1,17 @@
 import React from 'react'
+import { ToDoListItem } from './ToDoListItem'
 
 export const ToDoList = ({ toDos, handleToggle, handleDelete }) => {
     return (
         <ul className='list-group list-group-flush'>
         {
             toDos.map((toDo, i) => (
-                //* ToDoListItem, (toDos, index, handleDelete, handleToggle) */ }
-                
-                <li key={toDo.id} 
-                    className='list-group-item'>
-                    <p onClick={() => handleToggle(toDo.id)}
-                       className={ `${toDo.done && 'complete'}`}>
-                        {i + 1}. {toDo.desc}
-                    </p>
-                    <button className='btn btn-danger'
-                            onClick={ () => handleDelete(toDo.id) }>
-                        Delete
-                    </button>
-                </li>
-            ))
+                <ToDoListItem  toDo={toDo}
+                               i={i}
+                               key={toDo.id} 
+                               handleDelete={handleDelete}
+                               handleToggle={handleToggle}/>)
+            )
         }
         </ul>
     )
