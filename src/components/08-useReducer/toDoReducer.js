055a94885c57aada636abca34c765f1824ce2305
Cabrera Navarro, Kevin Oswaldo <kevin.cabrera@astrazeneca.com>
2022-01-04 21:53:15 -0600

    reduce complexity of map

--- src/components/08-useReducer/toDoReducer.js
+++ src/components/08-useReducer/toDoReducer.js
@@ -5,7 +5,7 @@ export const toDoReducer = ( state = [], action ) => {
             return [...state, action.payload];
         case 'delete':
             return state.filter((toDo) => toDo.id !== action.payload);
-        case 'toggle': 
+        case 'toggle-old': 
             return state.map(toDo => {
                 if (toDo.id === action.payload) {
                     return {
@@ -16,6 +16,10 @@ export const toDoReducer = ( state = [], action ) => {
                     return toDo;
                 }
             });
+        case 'toggle': 
+            return state.map(toDo => (toDo.id === action.payload) 
+            ? { ...toDo, done: !toDo.done } 
+            : toDo);
         default:
             return state;
     }
